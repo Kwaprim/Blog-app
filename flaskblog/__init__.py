9fff6e7e3c3b4a3d8c7f06dce3543e06d3913cbf
Prince K Millidah <princemillidah21@gmail.com>
2023-07-16 03:41:37 -0400

    modifications made

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -7,4 +7,4 @@ app.config['SECRET_KEY'] = '6a7bd560cad038cea4053de73c5cf6f5'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
-from firstapp import routes
+from flaskblog import routes
--- run_app.py
+++ run_app.py
@@ -1,4 +1,4 @@
-from firstapp import app
+from flaskblog import app
 
 if __name__ == "__main__":
     app.run(debug = True)
