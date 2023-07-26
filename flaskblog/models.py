d37bbc4753a7033dca7ad74b0b303ad383949cff
Prince K Millidah <princemillidah21@gmail.com>
2023-07-25 20:15:03 -0400

    Blueprint

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,7 +1,9 @@
-from datetime import datetime
+import datetime
 from itsdangerous import URLSafeTimedSerializer as Serializer
-from flaskblog import db, login_manager, app
+from flaskblog import db, login_manager
+from flask import current_app
 from flask_login import UserMixin
+import secrets
 
 
 @login_manager.user_loader
@@ -18,17 +20,23 @@ class User(db.Model, UserMixin):
     posts = db.relationship('Post', backref='author', lazy=True)
     
     def get_reset_token(self, expires_sec=300):
-        s = Serializer(app.config['SECRET_KEY'], expires_sec) # create a serializer object
-        return s.dumps({'user_id': self.id}).decode('utf-8') # return a token
-    
+        token = secrets.token_urlsafe(32)  # Generates a URL-safe token with 32 bytes of randomness
+        expiration_time = datetime.datetime.utcnow() + datetime.timedelta(seconds=expires_sec)
+        return token, expiration_time
    
     @staticmethod # static method does not take self as an argument
     def verify_reset_token(token):
-        s = Serializer(app.config['SECRET_KEY'])
+        s = Serializer(current_app.config['SECRET_KEY'])
         try:
-            user_id = s.loads(token)['user_id'] # try to load the token
+            data = s.loads(token)  # Try to load the token
+            user_id = data['user_id']
+            expiration_time = data.get('expiration_time') # try to load the token
         except:
             return None
+        
+        if expiration_time is not None and datetime.datetime.utcnow() > expiration_time:
+            return None
+        
         return User.query.get(user_id) # return the user id if the token is valid
 
     def __repr__(self):
@@ -38,7 +46,7 @@ class User(db.Model, UserMixin):
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(100), nullable = False)
-    date_posted = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+    date_posted = db.Column(db.DateTime, nullable = False, default = datetime.datetime.utcnow)
     content = db.Column(db.Text, nullable = False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
